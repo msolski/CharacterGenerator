71f944686d64cbcaf360b1287d52407592dc24be
michaelsolski <michaelavsolski@rogers.com>
2017-07-08 18:40:16 -0400

    Starting

---
+++ Documents/C Projects/info.h
@@ -0,0 +1,9 @@
+//Stats order:
+//Str, Dex, Con, Int, Wis, Cha
+
+typedef struct raceStruct {
+	char name[50];
+} race;
+
+race races[] = {{"Dragonborn"}, {"Dwarf"}, {"Elf"}, {"Gnome"}, {"Half-elf"}, 
+				{"Half-orc"}, {"Halfling"}, {"Human"}, {"Tiefling"}};
